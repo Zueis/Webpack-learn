884216fc6329723e77348681632513f8ceb6c4ac
zuies <zurabichkhikvadze66@gmail.com>
2022-10-23 18:37:32 +0400

    add live-server on dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development", // production,development
   entry: {
     bundle: path.resolve(__dirname, "src/index.js"), // which file we need to transpile
-
   },
   output: {
     path: path.resolve(__dirname, "dist"), // directory we need to build our output file
     filename: "[name].js", // name of output file
   },
-  module:{
-    rules:[
-      {
-        test:/\.scss$/,
-        use:['style-loader','css-loader','sass-loader']
-      },
-    ]
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback:true
   },
-  plugins:[
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-      title:'Webpack App',
-      filename:'index.html',
-      template:'src/template.html'
-    })
-  ]
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
 };
