bc8b1ca4e854dc995858bf7bb710e23dcffe0b1d
zuies <zurabichkhikvadze66@gmail.com>
2022-10-23 18:49:29 +0400

    add other rules to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,22 +8,39 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"), // directory we need to build our output file
     filename: "[name].js", // name of output file
+    clean: true, // keep one file on dist
+    assetModuleFilename: "[name][ext]",
   },
+  devtool: "source-map", // generate js.map source file for helping to debugg
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback:true
+    port: 3000, // serve live on port
+    open: true, // should browser open on run dev ?
+    hot: true, // hot realoading
+    compress: true, // gzip,etc
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // don't mess with node_module
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpeg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
