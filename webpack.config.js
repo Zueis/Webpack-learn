9e6c0f99b23a1ff6fd0efef6472bc7cc9c16fd63
zuies <zurabichkhikvadze66@gmail.com>
2022-10-23 18:06:20 +0400

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+module.exports = {
+  mode: "development", // production,development
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"), // which file we need to transpile
+
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"), // directory we need to build our output file
+    filename: "[name].js", // name of output file
+  },
+};
